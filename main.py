d2aa023cc22d3e51edc3a1c97294e0a3bedf1a6a
임재환 <jh.lim@piolink.com>
2022-07-29 18:34:36 +0900

    argparse 기능 추가

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import argparse
 from server import get_server_thread
 from peergroup import Peers, Peer, get_peer_group_thread
 
@@ -8,11 +10,21 @@ PORT = 3769
 
 
 if __name__ == "__main__":
-    myself = Peer(IP, PORT)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--me', '-m', type=str, help='--me 192.168.100.1:3769')
+    parser.add_argument('--peer', '-p', type=str, nargs='*', help='--peer 192.168.100.2:3769 192.168.100.3:3768')
+    args = parser.parse_args()
+    print(args.me)
+    print(args.peer)
+
+    ip, port = args.me.split(':')
+    print("My IP:{}, Port:{}".format(ip, port))
+    myself = Peer(ip, int(port))
     others = Peers()
-    others.add_new_peer('172.17.0.3', PORT)
-    others.add_new_peer('172.17.0.4', PORT)
-    others.add_new_peer('172.17.0.5', PORT)
+    for peer in args.peer:
+        ip, port = peer.split(':')
+        print("Peer IP:{}, Port:{}".format(ip, port))
+        others.add_new_peer(ip, int(port))
 
     peergroup_th = get_peer_group_thread(myself, others)
     server_th = get_server_thread(myself, peergroup_th)
